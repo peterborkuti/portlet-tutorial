9eb09b1da3bb6ec749c3744a74841c2b59324f68
Peter Borkuti <peter.borkuti@liferay.com>
2016-04-18 10:30:12 +0200

    copy code from EditServerAction.java

---
+++ scriptpp-portlet/docroot/WEB-INF/src/hu/borkuti/peter/scriptpp/portlet/ScriptppPortlet.java
@@ -0,0 +1,89 @@
+package hu.borkuti.peter.scriptpp.portlet;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletContext;
+import javax.portlet.PortletException;
+
+import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
+import com.liferay.portal.kernel.io.unsync.UnsyncPrintWriter;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.log.SanitizerLogWrapper;
+import com.liferay.portal.kernel.scripting.ScriptingException;
+import com.liferay.portal.kernel.scripting.ScriptingHelperUtil;
+import com.liferay.portal.kernel.scripting.ScriptingUtil;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.UnsyncPrintWriterPool;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+public class ScriptppPortlet extends MVCPortlet {
+
+	public void executeScript(ActionRequest actionRequest, ActionResponse actionResponse)
+			throws IOException, PortletException {
+
+		PortletConfig portletConfig = 
+			(PortletConfig)actionRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_CONFIG);
+
+		String script = ParamUtil.getString(actionRequest, "script", "");
+
+		_log.error("executeScript:" + script);
+
+		runScript(portletConfig, actionRequest, actionResponse);
+
+		PortalUtil.copyRequestParameters(actionRequest, actionResponse);
+	}
+
+	protected void runScript(
+			PortletConfig portletConfig, ActionRequest actionRequest,
+			ActionResponse actionResponse) {
+
+		String language = "groovy";
+		String script = ParamUtil.getString(actionRequest, "script");
+
+		PortletContext portletContext = portletConfig.getPortletContext();
+
+		Map<String, Object> portletObjects =
+			ScriptingHelperUtil.getPortletObjects(
+				portletConfig, portletContext, actionRequest, actionResponse);
+
+		UnsyncByteArrayOutputStream unsyncByteArrayOutputStream =
+			new UnsyncByteArrayOutputStream();
+
+		UnsyncPrintWriter unsyncPrintWriter = UnsyncPrintWriterPool.borrow(
+			unsyncByteArrayOutputStream);
+
+		portletObjects.put("out", unsyncPrintWriter);
+
+		try {
+			ScriptingUtil.exec(
+				null, portletObjects, language, script, StringPool.EMPTY_ARRAY);
+
+			unsyncPrintWriter.flush();
+
+			SessionMessages.add(
+				actionRequest, "scriptOutput",
+				unsyncByteArrayOutputStream.toString());
+		}
+		catch (ScriptingException se) {
+			SessionErrors.add(
+				actionRequest, ScriptingException.class.getName(), se);
+
+			Log log = SanitizerLogWrapper.allowCRLF(_log);
+
+			log.error(se.getMessage());
+		}
+	}
+
+	private Log _log = LogFactoryUtil.getLog(getClass());
+}
--- scriptpp-portlet/docroot/view.jsp
+++ scriptpp-portlet/docroot/view.jsp
@@ -1,21 +1,37 @@
+
 <%
-/**
- * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
- *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
- *
- *
- *
- */
+	/**
+	 * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+	 *
+	 * The contents of this file are subject to the terms of the Liferay Enterprise
+	 * Subscription License ("License"). You may not use this file except in
+	 * compliance with the License. You can obtain a copy of the License by
+	 * contacting Liferay, Inc. See the License for the specific language governing
+	 * permissions and limitations under the License, including but not limited to
+	 * distribution rights of the Software.
+	 *
+	 *
+	 *
+	 */
 %>
 
-<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet"%>
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui"%>
 
 <portlet:defineObjects />
 
-This is the <b>script++</b> portlet.
+This is the
+<b>script++</b>
+portlet.
+
+<portlet:actionURL name="executeScript" var="actionURL"></portlet:actionURL>
+
+<
+<aui:form action="<%=actionURL%>" method="POST">
+	<aui:fieldset>
+		<aui:input name="script" type="textarea"></aui:input>
+	</aui:fieldset>
+	<aui:button-row>
+		<aui:button type="submit" value="submit"></aui:button>
+	</aui:button-row>
+</aui:form>
